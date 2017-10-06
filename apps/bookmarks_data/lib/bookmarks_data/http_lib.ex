6d0f571e78a6e9d261de05e42e7a61b1cbc173ac
Jeffrey Chupp <jeff@semanticart.com>
2017-10-06 10:21:47 -0400

    Automatically ungzip when title-scraping

--- apps/bookmarks_data/lib/bookmarks_data/http_lib.ex
+++ apps/bookmarks_data/lib/bookmarks_data/http_lib.ex
@@ -8,9 +8,27 @@ defmodule BookmarksData.HTTPLib do
 
   def get(nil), do: {:ok, nil}
   def get(uri) do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} =
+    {:ok, response = %HTTPoison.Response{status_code: 200}} =
       HTTPoison.get(uri, [{"User-Agent", @user_agent}], @options)
 
-    {:ok, body}
+    {:ok, extract_body(response)}
+  end
+
+  # via https://github.com/edgurgel/httpoison/issues/81
+  defp extract_body(response) do
+    gzipped = Enum.any?(response.headers, fn (kv) ->
+      case kv do
+        {"Content-Encoding", "gzip"} -> true
+        {"Content-Encoding", "x-gzip"} -> true
+        _ -> false
+      end
+    end)
+
+    # return an Elixir string
+    if gzipped do
+      :zlib.gunzip(response.body)
+    else
+      response.body
+    end
   end
 end
